9a601056c82369ae3742339b6a1df9727988d433
Joshua Madin <jmadin@hawaii.edu>
2020-09-07 17:01:09 -1000

    Random added.

--- analysis.R
+++ analysis.R
@@ -1,2 +1,15 @@
 # Anyalsis
 
+# points selection
+
+n <- 100 # total species
+s <- 20  # species selected
+dat <- data.frame(x=rnorm(n), y=rnorm(n))
+plot(y ~ x, dat, col="grey")
+
+# random
+points(y ~ x, dat[sample(1:n, s, replace=FALSE),], col="red")
+
+# uniform
+
+# evenly spaced
